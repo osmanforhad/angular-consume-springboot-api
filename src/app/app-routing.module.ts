1076ca4daa0c58a5835b433331ae5236d6aa5cb4
osmanforhad <developer.osmanforhad@gmail.com>
2022-05-29 19:33:05 +0600

    configur angular router and setup navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EmployeelistComponent } from './employeelist/employeelist.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "employees", component: EmployeelistComponent},
+  {path: "", redirectTo: "employees", pathMatch: "full"}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
